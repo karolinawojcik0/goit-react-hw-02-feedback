4574f4297718f77dd33c741cc58fd43389e55384
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-04-10 18:45:01 +0200

    Task 2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -22,8 +22,22 @@ class Feedback extends Component {
     }));
   };
 
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state.stats;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state.stats;
+    const total = this.countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
   render() {
     const { stats } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+
     return (
       <>
         <h1>Please leave feedback</h1>
@@ -40,6 +54,8 @@ class Feedback extends Component {
         <p>Good: {stats.good}</p>
         <p>Neutral: {stats.neutral}</p>
         <p>Bad: {stats.bad}</p>
+        <p>Total: {totalFeedback}</p>
+        <p>Positive Feedback Percentage: {positiveFeedbackPercentage}%</p>
       </>
     );
   }
