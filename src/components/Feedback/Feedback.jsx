e74a33e90fa95a8b5ecb36e0dd8f6a39e0218520
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-04-10 09:19:28 +0200

    upd

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-export const Feedback = () => {
-  const [state, setState] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+// export const Feedback = () => {
+//   const [state, setState] = useState({
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   });
 
-  return (
-    <>
-      <h1>Please leave feeback</h1>
-      <button className="" type="button" onClick={() => handleFeedback('good')}>
-        Good
-      </button>
-      <button
-        className=""
-        type="button"
-        onClick={() => handleFeedback('neutral')}
-      >
-        Neutral
-      </button>
-      <button className="" type="button" onClick={() => handleFeedback('bad')}>
-        Bad
-      </button>
-      <h2>Statistics</h2>
-      <p>Good: {state.good}</p>
-      <p>Neutral: {state.neutral}</p>
-      <p>Bad: {state.bad}</p>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <h1>Please leave feeback</h1>
+//       <button className="" type="button" onClick={() => handleFeedback('good')}>
+//         Good
+//       </button>
+//       <button
+//         className=""
+//         type="button"
+//         onClick={() => handleFeedback('neutral')}
+//       >
+//         Neutral
+//       </button>
+//       <button className="" type="button" onClick={() => handleFeedback('bad')}>
+//         Bad
+//       </button>
+//       <h2>Statistics</h2>
+//       <p>Good: {state.good}</p>
+//       <p>Neutral: {state.neutral}</p>
+//       <p>Bad: {state.bad}</p>
+//     </>
+//   );
+// };
